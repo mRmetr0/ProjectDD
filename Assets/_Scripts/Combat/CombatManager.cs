7dadef25635cbe9888593bb660a5f46e7a5217d8
mRmetr0 <rwilookspelen@hotmail.com>
2024-12-20 18:25:23 +0100

    Add blender models, add combat manager

---
+++ Assets/_Scripts/Combat/CombatManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] PlayerPos;
+    [SerializeField] private GameObject[] EnemyPos;
+    
+}
